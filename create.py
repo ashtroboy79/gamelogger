dd7f7af1cd0522093856dd906ac9a9c96a1cee0d
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-03 14:53:10 +0000

    AR: trying to get deployment to work"

--- create.py
+++ create.py
@@ -1,14 +1,14 @@
 from application import db
 from application.models import Gamer, Game
 
-db.drop_all()
+# db.drop_all()
 db.create_all()
 
-bob = Gamer(name='Bob')
-ben = Gamer(name='Ben')
-joe = Gamer(name='Joe')
-game1 = Game(name='Argent The Consortium',rating=0, gamerbr=bob)
-game2 = Game(name="Revolution", designer='Steve Jackson',rating=0, gamerbr=bob)
-game3 = Game(name='Neanderthal', rating=0,gamerbr=ben)
-db.session.add_all([bob,ben,joe,game1,game2,game3])
-db.session.commit()
+# bob = Gamer(name='Bob')
+# ben = Gamer(name='Ben')
+# joe = Gamer(name='Joe')
+# game1 = Game(name='Argent The Consortium',rating=0, gamerbr=bob)
+# game2 = Game(name="Revolution", designer='Steve Jackson',rating=0, gamerbr=bob)
+# game3 = Game(name='Neanderthal', rating=0,gamerbr=ben)
+# db.session.add_all([bob,ben,joe,game1,game2,game3])
+# db.session.commit()
