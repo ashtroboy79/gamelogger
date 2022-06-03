18d62f4e7d363ba20238313ec51fe4f0ea9896cb
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-03 13:20:32 +0000

    AR:Added Deployment step

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# Installing packages
 sudo apt install python3 python3-pip python3-venv -y
+# Setting up vene
 python3 -m venv venv
 source venv/bin/activate
+# Installing dependancies
 pip3 install -r requirements.txt
+# Running unit tests
 python3 -m pytest --cov=application --cov-report=html
+# Copying files
+scp -r . jenkins@app-server:/home/jenkins/app
+# Deploying app
+ssh jenkins@app-server < deploy.sh
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install python3 python3-pip python3-venv -y
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3 app.py
