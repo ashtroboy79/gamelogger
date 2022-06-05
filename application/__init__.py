d38ea2802d020b5fe253e1b1513bf1ef91a9257f
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-05 13:54:24 +0000

    AR:Trying deployemnt step on new app-server

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #os.getenv("GAMELOGGER_DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #getenv("GAMELOGGER_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = getenv("GAMELOGGER_SECRET_KEY")
 db = SQLAlchemy(app)
