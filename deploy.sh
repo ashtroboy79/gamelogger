f90e578193dd7e7976ebf75b030437c76997838c
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-06 18:41:29 +0000

    AR:checking that the deploy.sh is working

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,4 @@ sudo kill -9 $(sudo lsof -i :5000 | cut -d' ' -f2 | tail -n 5 | head -n 1)
 python3 create.py
 echo "gunicorn"
 python3 -m gunicorn -D --workers 4 --bind 0.0.0.0:5000 app:app 
+# adding comment to check deployment is working
