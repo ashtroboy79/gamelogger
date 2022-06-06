277977f04da05e1fc284f60411cbb353fbfe3cdf
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-06 17:59:38 +0000

    AR:adding gunicorn to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt update
-sudo apt install python3 python3-pip python3-venv -y
+sudo apt install python3 python3-pip python3-venv gunicorn -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 python3 create.py
-python3 app.py
+gunicorn --workers 4 --bind 0.0.0.0:5000 app:app 
