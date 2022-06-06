831447a3d3abc678f238ea2761e98838db900746
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-06 18:25:24 +0000

    AR:fixing gunicorn deployment by adding gunicorn to requirements.txt to be installed by pip, and using -D to detach the process, and adding kill command to kill the process on 5000

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt update
-sudo apt install python3 python3-pip python3-venv gunicorn -y
+sudo apt install python3 python3-pip python3-venv -y
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
+sudo kill -9 $(sudo lsof -i :5000 | cut -d' ' -f2 | tail -n 5 | head -n 1)
 python3 create.py
-gunicorn --workers 4 --bind 0.0.0.0:5000 app:app 
+python3 -m gunicorn -D --workers 4 --bind 0.0.0.0:5000 app:app 
