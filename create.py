fb9729c6e20634aba614cef8c3fb7a70c81a9c2b
ahsanrasul <rasul.ahsan@gmail.com>
2022-06-16 10:17:03 +0000

    AR: Added tests and functionality to display games by a particular designer

--- create.py
+++ create.py
@@ -2,6 +2,15 @@ from application import db
 from application.models import Gamer, Game
 
 if __name__ == '__main__':
+    db.drop_all()
     db.create_all()
 
+    bob = Gamer(name='Bob The Builder')
+    joe = Gamer(name='Joe Blogs')
+    ben = Gamer(name='Ben')
+    game1 = Game(name='Argent The Consortium',rating=0, gamerbr=bob)
+    game2 = Game(name="Revolution", designer='Steve Jackson',rating=0, gamerbr=bob)
+    game3 = Game(name='Neanderthal', rating=0,gamerbr=ben)
+    db.session.add_all([bob,joe,ben,game1,game2,game3])
+    db.session.commit()
  
